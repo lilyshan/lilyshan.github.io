2804164589d97e107aebed2d831b9a61cede240f
Lily Shan <lilys@andrew.cmu.edu>
2020-04-14 00:45:37 -0400

    clean up and updated HW8

---
+++ HW8/portfolio.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.single-item').slick({
+  	dots:true,
+  });
+}); 
+
+/*
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+}); 
+
+
+$(document).on('ready', function() {
+      $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+    });
+
+*/
